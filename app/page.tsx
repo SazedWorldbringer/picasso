950c6e8f97a4269d44394ab8f5b7707645ebe0cd
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-11 19:56:14 +0530

    feat: use react-color for picking colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { useDraw } from "@/hooks/use-draw";
+import { useState } from "react";
 import { FC } from "react";
+import { ChromePicker } from "react-color";
 
 interface pageProps { }
 
 const page: FC<pageProps> = ({ }) => {
+  const [color, setColor] = useState<string>('#000')
   const { canvasRef, onMouseDown } = useDraw(drawLine)
 
   // argument order doesn't matter when they're in curly braces
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
     const { x: currX, y: currY } = currentPoint
-    const lineColor = '#000'
+    const lineColor = color
     const lineWidth = 5
 
     const startPoint = prevPoint ?? currentPoint
@@ -30,6 +33,7 @@ const page: FC<pageProps> = ({ }) => {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
+      <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
       <canvas
         onMouseDown={onMouseDown}
         ref={canvasRef}
